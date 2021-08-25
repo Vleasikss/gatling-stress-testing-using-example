fcc55666c82c176565e4e6f149c806fbff47df53
Vlas Bieliaiev <vlas.bieliaiev@nixsolutions.com>
2021-08-25 17:07:55 +0300

    INIT

---
+++ src/test/scala/simulations/CreateUserSimulation.scala
@@ -0,0 +1,26 @@
+package simulations
+
+import io.gatling.core.scenario.Simulation
+
+import io.gatling.core._
+import io.gatling.http._
+
+/**
+ * @see <a href="https://reqres.in/"> fake data generator </a>
+ */
+class CreateUserSimulation extends Simulation {
+
+  private val URL = "https://reqres.in/"
+
+  //protocol
+
+
+  //define requests
+
+
+  //setup scenario
+
+
+  //inject users
+
+}
